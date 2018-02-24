a02d1322b559afdb1b66769be0cd3a1ee9a45efc
josman958 <josue_luistj@hotmail.com>
2018-02-24 04:48:43 -0500

    Add files via upload

---
+++ reto3-web3/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</head>
+<body>
+
+	<div id="header">
+	    <div class="navbar-fixed">
+	        <nav class="orange">
+	            <div class="nav-wrapper container">
+	                <a href="#!" class="brand-logo">&nbsp;El header de Josss</a>
+	            </div>
+	        </nav>
+	    </div>
+	</div>
+	<div class="row">
+		<div class="col s3" id="nav">
+			<ul id="slide-out" class="side-nav fixed" style="margin-top: 70px;">
+			    <li>
+			      <div class="userView">
+			        <a href="#!user"><img class="circle" src="https://scontent.flim5-4.fna.fbcdn.net/v/t31.0-8/12622346_1133770133330339_6584012604996366779_o.jpg?_nc_eui2=v1%3AAeHPpDFCHRhsJ-iDmXPwAxj32R5k89MxGGScjxZu_B9GdDBxLu905QIBDR0hgSXwwsM5tP4UsuNzKR2JKA5bPUWssHa2JLLlxA8biLNk6ELv9A&oh=62a6743b4a52fe4ab2809bf603e4b930&oe=5B05D689"></a>
+			      </div>
+			    </li>
+			    <li><a href="#"><i class="material-icons">cloud</i>Mi perfil joss</a></li>
+			    <li><div class="divider"></div></li>
+			    <li><a class="subheader">Descripcion</a></li>
+			    <li><a class="waves-effect" href="#">Esta pagina es de prueba</a></li>
+			</ul>
+		</div>
+		<div class="col s9">
+			<main>
+				<article>
+					<header>
+						<div class="row">
+					      <div class="col s12 m12">
+					        <div class="card-panel teal">
+					          <h2 class="white-text">primer articulo 
+					          </h2> 
+					          <time>20-02-2018</time>
+					        </div>
+					      </div>
+					    </div>
+				    </header>
+				    <p><div class="card-panel hoverable"> Contenido del articulo</div></p>
+				    <footer class="card-panel z-depth-1 page-footer center"><a href="https://www.facebook.com/josueluis.mancilla">facebook</a>, <a href="https://www.facebook.com/josueluis.mancilla">facebook</a>, <a href="https://www.facebook.com/josueluis.mancilla">facebook</a></footer>
+				    <div class="comentarios">
+	                    <article>
+	                        <time>Enviado el 20-02-2018 a las 5:35 pm</time>
+	                        <p>Este es un muy buen comentario sobre el artículo.</p>
+	                    </article>
+	                    <article>
+	                        <time>Enviando el 20-02-2018 a las 8:12 pm</time>
+	                        <p>Este es un mal comentario :(</p>
+	                    </article>
+				        <div class="input-field col s12">
+				          <input id="comente" type="text" class="validate">
+				          <label for="comente">Comente</label>
+				        </div>
+	                    
+	                    <a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>Enviar comentario</a>
+                	</div>
+			    </article>
+			    <hr>
+			    <article>
+					<header>
+						<div class="row">
+					      <div class="col s12 m12">
+					        <div class="card-panel teal">
+					          <h2 class="white-text">Segundo Articulo
+					          </h2> 
+					          <time>20-02-2018</time>
+					        </div>
+					      </div>
+					    </div>
+				    </header>
+				    <p><div class="card-panel hoverable"> Contenido del segundo articulo</div></p>
+				    <footer class="card-panel z-depth-1 page-footer center"><a href="https://www.facebook.com/josueluis.mancilla">facebook</a>, <a href="https://www.facebook.com/josueluis.mancilla">facebook</a>, <a href="https://www.facebook.com/josueluis.mancilla">facebook</a></footer>
+				    <div class="comentarios">
+	                    <article>
+	                        <time>Enviado el 20-02-2018 a las 5:35 pm</time>
+	                        <p>Este es un muy buen comentario sobre el artículo.</p>
+	                    </article>
+	                    <article>
+	                        <time>Enviando el 20-02-2018 a las 8:12 pm</time>
+	                        <p>Este es un mal comentario :(</p>
+	                    </article>
+				        <div class="input-field col s12">
+				          <input id="comente" type="text" class="validate">
+				          <label for="comente">Comente</label>
+				        </div>
+	                    
+	                    <a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>Enviar comentario</a>
+                	</div>
+			    </article>
+			   	<hr>
+			    <article>
+					<header>
+						<div class="row">
+					      <div class="col s12 m12">
+					        <div class="card-panel teal">
+					          <h2 class="white-text">Tercer Articulo
+					          </h2> 
+					          <time>20-02-2018</time>
+					        </div>
+					      </div>
+					    </div>
+				    </header>
+				    <p><div class="card-panel hoverable"> Contenido del tercer articulo</div></p>
+				    <footer class="card-panel z-depth-1 page-footer center"><a href="https://www.facebook.com/josueluis.mancilla">facebook</a>, <a href="https://www.facebook.com/josueluis.mancilla">facebook</a>, <a href="https://www.facebook.com/josueluis.mancilla">facebook</a></footer>
+				    <div class="comentarios">
+	                    <article>
+	                        <time>Enviado el 20-02-2018 a las 5:35 pm</time>
+	                        <p>Este es un muy buen comentario sobre el artículo.</p>
+	                    </article>
+	                    <article>
+	                        <time>Enviando el 20-02-2018 a las 8:12 pm</time>
+	                        <p>Este es un mal comentario :(</p>
+	                    </article>
+				        <div class="input-field col s12">
+				          <input id="comente" type="text" class="validate">
+				          <label for="comente">Comente</label>
+				        </div>
+	                    
+	                    <a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>Enviar comentario</a>
+                	</div>
+			    </article>
+			</main>
+		</div>
+	</div>
+	<footer class="card-panel z-depth-1 page-footer center" style="color:  black;">Este es el footer de toda la página.</footer>
+	<script>
+		 // SIDEBAR
+$(document).ready(function(){
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+  // START OPEN
+  $('.button-collapse').sideNav('show');
+});
+	</script>
+</body>
+</html>
